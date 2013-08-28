bded4d9a6bd743d46db1613a5c50afe5dcf062be
natewalck <nate@osxadm.in>
2013-08-28 19:57:33 -0400

    Fix paths in vmware.sh to mirror munki user

--- centos-6.4-x86_64-netboot/scripts/vmware.sh
+++ centos-6.4-x86_64-netboot/scripts/vmware.sh
@@ -5,15 +5,14 @@
 
 # Install the VMWare Tools from a linux ISO.
 
-#wget http://192.168.0.185/linux.iso -P /tmp
 mkdir -p /mnt/vmware
-mount -o loop /home/vagrant/linux.iso /mnt/vmware
+mount -o loop /home/munki/linux.iso /mnt/vmware
 
 cd /tmp
 tar xzf /mnt/vmware/VMwareTools-*.tar.gz
 
 umount /mnt/vmware
-rm -fr /home/vagrant/linux.iso
+rm -fr /home/munki/linux.iso
 
 /tmp/vmware-tools-distrib/vmware-install.pl -d
 rm -fr /tmp/vmware-tools-distrib
